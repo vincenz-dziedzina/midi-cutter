1a0008f9c455315c3fecb64cf77f911bff90a4b5
flintzer <smoelgebroed@gmail.com>
2018-11-19 18:31:18 +0100

    Extract first 2 tracks from 'mysong.mid' and create 'newsong.mid' with them.

--- Assets/Scripts/testScript.cs
+++ Assets/Scripts/testScript.cs
@@ -1,25 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Commons.Music.Midi;
+using System.IO;
 
-public class testScript : MonoBehaviour {
+public class testScript : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		var access = MidiAccessManager.Default;
-		var output = access.OpenOutputAsync(access.Outputs.Last().Id).Result;
-		output.Send(new byte [] {0xC0, GeneralMidi.Instruments.AcousticGrandPiano}, 0, 2, 0); // There are constant fields for each GM instrument
-		output.Send(new byte [] {MidiEvent.NoteOn, 0x40, 0x70}, 0, 3, 0); // There are constant fields for each MIDI event
-		output.Send(new byte [] {MidiEvent.NoteOff, 0x40, 0x70}, 0, 3, 0);
-		output.Send(new byte [] {MidiEvent.Program, 0x30}, 0, 2, 0); // Strings Ensemble
-		output.Send(new byte [] {0x90, 0x40, 0x70}, 0, 3, 0);
-		output.Send(new byte [] {0x80, 0x40, 0x70}, 0, 3, 0);
-		output.CloseAsync();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        //var music = MidiMusic.Read(System.IO.File.OpenRead("mysong.mid"));
+        //var player = new MidiPlayer(music, output);
+        //player.EventReceived += (MidiEvent e) => {
+        //    if (e.EventType == MidiEvent.Program)
+        //        Debug.Log($"Program changed: Channel:{e.Channel} Instrument:{e.Msb}");
+        //};
+        //player.PlayAsync();
+        SmfReader reader = new SmfReader();
+        reader.Read(System.IO.File.OpenRead("mysong.mid"));
+        var music = reader.Music;
+        var tracks = music.Tracks;
+        var splitMusic = SmfTrackSplitter.Split(tracks[0].Messages, music.DeltaTimeSpec);
+        byte[] data = splitMusic.Tracks[0].Messages[0].Event.Data;
+        var stream = File.Create("newsong.mid");
+        SmfWriter writer = new SmfWriter(stream);
+        writer.WriteHeader(music.Format, /*(short)music.Tracks.Count*/2, music.DeltaTimeSpec);
+        writer.WriteTrack(tracks[0]);
+        writer.WriteTrack(tracks[1]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
