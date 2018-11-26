1a45da66a2541d8a0d70193a27c22b36ec1106c2
Vincenz Dziedzina <vincenz.dziedzina@gmail.com>
2018-11-26 15:21:35 +0100

    Add method for adding an SMPTE event and refactor existing code

--- Assets/Scripts/testScript.cs
+++ Assets/Scripts/testScript.cs
@@ -21,43 +21,56 @@ public class testScript : MonoBehaviour
         SmfWriter writer = new SmfWriter(stream);
         writer.WriteHeader(music.Format, (short)music.Tracks.Count, music.DeltaTimeSpec);
 
-        logMidiInformation(music);
+        LogMidiInformation(music);
 
         for (var i = 0; i < tracks.Count; i++)
         {
             var track = tracks[i];
             int passedTime = 0;
             var newTrack = new MidiTrack();
+
+            if(i == 0) {
+                AddSMPTEOffsetEvent(newTrack);
+            }
+
             for (var j = 0; j < track.Messages.Count; j++)
             {
                 var midiMessage = track.Messages[j];
 
                 passedTime += midiMessage.DeltaTime;
 
-                if(midiMessage.Event.EventType == MidiEvent.Meta || passedTime < 2000) {
+                if(midiMessage.Event.EventType == MidiEvent.Meta || passedTime < 20000) {
                     newTrack.AddMessage(midiMessage);
                 }
             }
 
             track = newTrack;
-            Debug.Log("Track " + tracks.IndexOf(track) + " Passed time:" + passedTime);
+            Debug.Log("Track " + (i + 1) + " Passed time:" + passedTime);
             writer.WriteTrack(track);
         }
-        this.AddEndOfTrackMessage(tracks[0]);
+        AddEndOfTrackMessage(tracks[0]);
         Debug.Log("DELTA TIME: " + music.DeltaTimeSpec);
         Debug.Log("DONE");
+    }
+
+    private void AddSMPTEOffsetEvent(MidiTrack track) {
+        track.AddMessage(new MidiMessage(0, new MidiEvent(
+            MidiEvent.Meta, 
+            MidiMetaType.SmpteOffset, 
+            (byte) 0x05, // length: 5 bytes will follow
+            new byte[] { 0x01, 0x00, 0x05, 0x00, 0x00 })));
     }
 
     /// Only call once per MIDI as this marks the end of the MIDI.
     /// To my knowledge it doesn't matter which track is passed.
     private void AddEndOfTrackMessage(MidiTrack track)
     {
-        var evt = new MidiEvent(12032); // 'FF 2F 00' -> end of track
+        var evt = new MidiEvent(MidiEvent.Meta, MidiMetaType.EndOfTrack, (byte) 0x00, null); // 'FF 2F 00' -> end of track
         var msg = new MidiMessage(0, evt);
         track.AddMessage(msg);
     }
 
-    private void logTracks(IList<MidiTrack> tracks) {
+    private void LogTracks(IList<MidiTrack> tracks) {
         for (int i = 0; i < tracks.Count; i++)
         {
             MidiTrack midiTrack = tracks[i];
@@ -72,8 +85,8 @@ public class testScript : MonoBehaviour
         }
     }
 
-    private void logMidiInformation(MidiMusic music) {
-        Debug.Log("Divisions: " + music.DeltaTimeSpec);
+    private void LogMidiInformation(MidiMusic music) {
+        Debug.Log("Divisions: " + System.Convert.ToString(music.DeltaTimeSpec, 2));
         Debug.Log("Format: " + music.Format);
         Debug.Log("Number of tracks: " + music.Tracks.Count);
         try
@@ -87,14 +100,14 @@ public class testScript : MonoBehaviour
        
     }
 
-    private void logMidiMessage(MidiMessage midiMessage) {
+    private void LogMidiMessage(MidiMessage midiMessage) {
         Debug.Log("MidiMessage:");
         Debug.Log("MidiMessage: " + midiMessage);
         Debug.Log("Delta time: " + midiMessage.DeltaTime);
-        logMidiEvent(midiMessage.Event);
+        LogMidiMessage(midiMessage.Event);
     }
 
-    private void logMidiEvent(MidiEvent midiEvent) {
+    private void LogMidiMessage(MidiEvent midiEvent) {
         Debug.Log("Event:");
         Debug.Log("Event type: " + System.String.Format("{0:X}", midiEvent.EventType));
         Debug.Log("Event Meta type: " + System.String.Format("{0:X}", midiEvent.MetaType));
